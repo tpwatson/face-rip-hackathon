07f21389c0c675fdc76a3c3c7ac4c0775db43f71
Terence Watson <terence.p.watson@gmail.com>
2022-10-24 13:52:15 -0700

    settings route

---
+++ src/routes/Settings.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+/*
+Include a way to seperate user's viewer face from their creator account.
+THis is for like bearable bull or other creators who remain anonymouse.
+
+Their face can be show via a seperate wallet from their creator profile.
+This is optional and up to the creator if they want to show their support of other creators or not.
+
+Unlink laughs from account, in case people don't want to show the things they laugh at evily lol
+
+If a person still doesn't want to show their face, put an option for face recog landmarks lines and dots only.
+Some people may really wish not to show their face for personal reasons.
+
+But should there be a penalty for not showing faces? Does this hurt the creator's incentives?
+
+*/
